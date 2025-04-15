e03a43b8ed03f1711769b85e6ecc204065a459de
Emmanuel-Howard <emmanuelhoward3000@gmail.com>
2025-04-14 20:04:58 -0700

    Learned java basics + Initial setup

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.util.Stack;
+
+public class Main {
+
+    // Stack = LIFO (Last In First Out)
+    // Push = Add an element to the top of the stack
+    // Pop = Remove the top element from the stack
+    // Peek = Get the top element without removing it
+
+    
+
+}

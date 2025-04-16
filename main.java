0e4b6c86d3005d6d8d162fe0de1a2bce62362172
Emmanuel-Howard <emmanuelhoward3000@gmail.com>
2025-04-15 20:46:16 -0700

    Learning Stacks DS

--- main.java
+++ main.java
@@ -1,11 +1,27 @@
 import java.util.Stack;
 
-public class Main {
+public class main {
 
-    // Stack = LIFO (Last In First Out)
-    // Push = Add an element to the top of the stack
-    // Pop = Remove the top element from the stack
-    // Peek = Get the top element without removing it
-    
+    public static void main(String[] args) {
+        
+        // Stack = LIFO (Last In First Out)
+        // Push = Add an element to the top of the stack
+        // Pop = Remove the top element from the stack
+        // Peek = Get the top element without removing it
 
+        Stack<String> stack = new Stack<String>(); // Create a stack of strings
+
+        stack.push("Minecraft");
+        stack.push("Bordelands");
+        stack.push("FIFA");
+
+        String favGame = stack.pop(); // Remove the top element (FIFA)
+        System.out.println(favGame); // Print the removed element (FIFA)
+        stack.push("Happy Wheels"); // Add a new element (Happy Wheels)
+
+        System.out.println(stack.empty()); // Check if the stack is empty
+        System.out.println(stack.peek()); // Check the head of the stack (Happy Wheels)
+
+        System.out.println(stack.search("Happy Wheels")); // Search for an element (Happy Wheels) and return its position from the top of the stack
+    }
 }

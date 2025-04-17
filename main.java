d1d00821d69532c6787b4a6fa0c9671f166adf23
Emmanuel-Howard <emmanuelhoward3000@gmail.com>
2025-04-16 20:46:50 -0700

    Learning Queues DS

--- main.java
+++ main.java
@@ -1,9 +1,11 @@
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class main {
 
     public static void main(String[] args) {
-        
+        /*
         // Stack = LIFO (Last In First Out)
         // Push = Add an element to the top of the stack
         // Pop = Remove the top element from the stack
@@ -23,5 +25,25 @@ public class main {
         System.out.println(stack.peek()); // Check the head of the stack (Happy Wheels)
 
         System.out.println(stack.search("Happy Wheels")); // Search for an element (Happy Wheels) and return its position from the top of the stack
+    ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        */
+    
+        // Queue = FIFO (First In First Out) data structure (Ex. Line of ppl). A linear data strucutre that stores items in the order they were added.
+        // Enqueue = Add an element to the end of the queue
+        // Dequeue = Remove the first element from the queue
+        // Peek = Get the first element without removing it
+
+        Queue<String> queue = new LinkedList<String>(); // Create a queue of strings
+
+        queue.add("Minecraft"); // Enqueue (add) an element to the end of the queue
+        queue.add("Bordelands"); // Enqueue (add) another element to the end of the queue
+        queue.add("FIFA"); // Enqueue (add) another element to the end of the queue
+
+        String favGame = queue.remove(); // Dequeue (remove) the first element from the queue (Minecraft)
+        System.out.println(favGame); // Print the removed element (Minecraft)
+
+        System.out.println(queue.peek()); // Peek at the first element in the queue (Bordelands)
+        System.out.println(queue.isEmpty()); // Check if the queue is empty (false)
+
     }
 }
